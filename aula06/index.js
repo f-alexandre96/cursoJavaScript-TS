d67b2873643dbb68de2883e687cbe5b70143d9a1
Fernando Alexandre da Silva <fernando17034@gmail.com>
2025-08-21 10:42:02 -0300

    como criar variaveis let

---
+++ aula06/index.js
@@ -0,0 +1,14 @@
+let nome = 'Fernando';
+
+let sobrenome; //declarou a variavel
+sobrenome = 'Silva'; // inicializei a variável
+
+console.log(nome, 'nasceu em 1996');
+console.log(sobrenome, 'é sobrenome');
+//nao podemos criar variaveis com palavras reservadas do JS
+//nao pode criar nome de variavel com numero
+//nao pode ter traço ou espaço no nome da variavel
+//usar camelCase  -> nomeCompletoDoCliente
+//variaveis sao case-sensitive, letra maiscula diferencia de minuscula
+//nao podemos redeclarar variaveis com let
+// não usar VAR, usar LET.
